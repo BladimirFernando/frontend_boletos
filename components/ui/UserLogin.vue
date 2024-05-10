9fe3826397d42ed70fd266e7868f9617ce2b711e
bladimirh179@gmail.com <bladimirh179@gmail.com>
2024-05-10 08:13:11 -0600

    primeros pasos de la pagina principal

---
+++ components/ui/UserLogin.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card rounded color="#EC9143" class="pa-7">
+    <v-card-title>
+      <v-row class="rowCard fontTitle" align="center">
+        Welcome
+      </v-row>
+      <v-row class="rowCard fontTitle" align="center">
+        Back
+      </v-row>
+    </v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-text-field v-model="email" rounded label="E-mail" outlined />
+      </v-row>
+      <v-row>
+        <v-text-field v-model="password" rounded label="Password" outlined type="password" />
+      </v-row>
+    </v-card-text>
+    <v-card-actions>
+      <v-col cols="12">
+        <v-row>
+          <v-btn block class="pa-2" color="#134700" @click="login">
+            <span style="text-transform: none; color: white;">Log-In</span>
+          </v-btn>
+        </v-row>
+        <v-row class="rowCard">
+          <span>If you Didn't have account</span>
+          <a href="">Sign-Up</a>
+        </v-row>
+      </v-col>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-img
+          :src="require('../assets/images/connectando.jpg')"
+          height="120px"
+          width="120px"
+          alt="Descripción de la imagen"
+          style="margin-left: 60px;"
+        />
+      </v-col>
+      <v-col>
+        <v-btn text class="black--text">
+          Inicio
+        </v-btn>
+        <v-btn text class="black--text">
+          Acerca de
+        </v-btn>
+        <v-btn text class="black--text">
+          Contacto
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card rounded color="white" class="custom-card" style="margin-left: 800px;">
+          <v-card-text>
+            <v-row>
+              <v-text-field v-model="email" label="Ingresa tu correo" outlined />
+            </v-row>
+            <v-row>
+              <v-text-field v-model="password" label="Ingresa tu contraseña" outlined type="password" />
+            </v-row>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn block class="pa-2" color="#33ffc7" @click="login">
+              <span style="text-transform: none; color: white;">Iniciar sesión</span>
+            </v-btn>
+            <v-row class="rowCard" justify="center" />
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+.custom-card {
+  max-width: 300px; /* Establecer el ancho máximo del v-card */
+  max-height: 300px; /* Establecer la altura máxima del v-card */
+  padding: 10px; /* Reducir el padding interno del v-card */
+  font-size: 14px; /* Reducir el tamaño de la fuente dentro del v-card */
+}
+</style>
